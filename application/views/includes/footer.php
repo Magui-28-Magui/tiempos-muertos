3c8c41d9000f0a515e44ee9ede4bdafd30631d13
Magui Campos <88505411+MaguiCampos@users.noreply.github.com>
2022-08-22 11:00:01 -0700

    change url

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -8,7 +8,7 @@
 
 <!-- Scripts -->
 <script src="<?php echo  base_url() ?>assets/js/bootstrap.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="<?php echo  base_url() ?>assets/datatables/DataTables-1.12.0/js/jquery.dataTables.min.js"></script>
 <script src="<?php echo  base_url() ?>assets/datatables/Buttons-2.2.3/js/dataTables.buttons.min.js"></script>
